1af1d9f7ec7f304e43f976d3bf0e5c1c2dbc10c6
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2026-04-02 18:01:55 +0600

    book poka running

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from "react-router";
 import Root from "../pages/Root/Root";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,10 @@ export const router = createBrowserRouter([
         path: "/",
         loader: () => fetch("./booksData.json"),
         Component: Home,
+      },
+      {
+        path: "/about",
+        Component: About,
       },
     ],
     errorElement: <ErrorPage></ErrorPage>,
--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -3,21 +3,34 @@ import { FaStarHalfStroke } from "react-icons/fa6";
 
 const Book = ({ book }) => {
   console.log(book);
-  const { bookName, author, image, publisher, rating, review, category } = book;
+  const {
+    bookName,
+    author,
+    image,
+    publisher,
+    rating,
+    review,
+    tags,
+    category,
+    yearOfPublishing,
+  } = book;
   return (
     <div className="card bg-base-100  shadow-md p-6 border border-gray-300">
       <figure className="bg-gray-200 w-2/3 mx-auto p-4">
         <img className="h-[166px]" src={image} alt="Shoes" />
       </figure>
       <div className="card-body">
+        <div className="flex justify-center gap-10">
+          {tags.map((tag) => (
+            <button>{tag}</button>
+          ))}
+        </div>
         <h2 className="card-title">
           {bookName}
-          <div className="badge badge-secondary">NEW</div>
+          <div className="badge badge-secondary">{yearOfPublishing}</div>
         </h2>
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
+        <p>Book by: {publisher}</p>
+        <div className="border-t border-dashed"></div>
         <div className="card-actions justify-end">
           <div className="badge badge-outline">{category}</div>
           <div className="badge badge-outline">
